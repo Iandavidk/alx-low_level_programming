d5196ed7c2b7a0235c5a58bd8cf27e3201a479e0
Iandavidk <eandavid6@gmail.com>
2022-06-07 15:13:57 -0700

    Minor modifications

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 #include <unistd.h>
 #include <ctype.h>
-#include <stdlib.h>
-#include <locale.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -74,18 +72,3 @@ return (1);
 return (0);
 }
 
-
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
